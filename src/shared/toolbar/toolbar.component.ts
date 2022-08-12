76899fb1bbaf735614cd5e52db5cdd5ae2c77146
sapphinebz <sapphinebz@gmail.com>
2022-08-12 16:04:42 +0700

    add bagde

--- src/shared/toolbar/toolbar.component.ts
+++ src/shared/toolbar/toolbar.component.ts
@@ -1,20 +1,26 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatBadgeModule } from '@angular/material/badge';
+
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatSidenavModule,
+  ],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
-  // @HostBinding('style.display') display = 'flex';
-  // @HostBinding('style.justify-content') justifyContent = 'space-between';
-  // display: flex;
-  // justify-content: space-between;
   constructor() {}
 
   ngOnInit(): void {}
